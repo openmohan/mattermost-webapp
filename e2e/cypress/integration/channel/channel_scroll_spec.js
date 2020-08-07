8aa7a3ed7858e93a6deff2c66db378bbc342cf6a
Mohan Prasath <openmohan@gmail.com>
2020-08-07 16:39:16 +0530

    MM-T2365

--- e2e/cypress/integration/channel/channel_scroll_spec.js
+++ e2e/cypress/integration/channel/channel_scroll_spec.js
@@ -1,6 +1,8 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
+import {callbackify} from 'util';
+
 // ***************************************************************
 // - [#] indicates a test step (e.g. # Go to a page)
 // - [*] indicates an assertion (e.g. * Check the title)
@@ -21,6 +23,26 @@ describe('Close direct messages', () => {
             // # Login as test user and go to town square
             cy.apiLogin(testUser);
             cy.visit(`/${testTeam.name}/channels/town-square`);
+        });
+    });
+
+    it('MM-T2365 Scrolling in the channel is disabled when emoji picker is open', () => {
+        // # Add 30 posts to create enough space from bottom for making channel scrollable
+        for (let index = 0; index < 3; index++) {
+            cy.postMessage(`This is an old message [${index}]`);
+        }
+
+        cy.get('#emojiPickerButton').should('be.visible').click();
+
+        cy.get('#root').should('have.class', 'emoji-picker--active');
+
+        cy.get('#emojiPickerButton').should('be.visible').click();
+
+        cy.get('#root').should('not.have.class', 'emoji-picker--active');
+
+        cy.getLastPostId().then((postId) => {
+            cy.clickPostDotMenu(postId);
+            cy.contains('Reply').click();
         });
     });
 });
